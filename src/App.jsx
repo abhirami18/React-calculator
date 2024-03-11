42d33aaba7c932256aa0b72f883dd76b842183a9
Abhirami Prakash <abhiramiharitha18@gmail.com>
2024-03-11 13:09:44 +0530

    simplecalculator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Calculator from './Components/Calculator'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div  className="App">
+        <Calculator />
+      </div>
+    </>
+  )
+}
+
+export default App
